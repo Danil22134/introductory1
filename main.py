b94af1c8c39ea61fd939145276e39e40e752fc4d
Danil22134 <kuchinskiy-danil7@mail.ru>
2024-06-06 00:44:40 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# 1 задание
+a = "Hello world"
+print(len(a))
+
+# 2 задание
+
+first = 5
+second = 9
+sum = first + second
+duff = first - second
+print(sum, duff)
+# 3 задание
+
+first = 7
+second = 5
+third = 3
+mean = ((first + second + third) / 3)
+print(mean)
+
+# 4
+
+first_string = "Tuesday"
+second_string = "Monday"
+print(second_string, ',' ,first_string)
+
+#5
+
+a = 2
+b = 5
+c = 8
+f = (((a*b) + (a*c)))
+print(f)
+print((f**3)/2)
